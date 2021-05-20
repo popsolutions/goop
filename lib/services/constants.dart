d83a36ca9783a9436584463c42c39f1e94205bf0
Ismael Pedro <http.ismaeel@gmail.com>
2021-05-20 20:55:07 -0300

    Adicionado constant pops.meassurement

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -2,4 +2,5 @@ class Strings {
   static const String resPartner = "res.partner";
   static const String resUsers = "res.users";
   static const String missions = 'pops.missions';
+  static const String meassurement = 'pops.meassurement';
 }
