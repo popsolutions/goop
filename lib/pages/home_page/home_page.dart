86e95ccd86de01f4229ea226cdd8c6929bb3d49e
Ismael Pedro <http.ismaeel@gmail.com>
2021-06-02 15:03:08 -0300

    readicionado o mapa da tela home

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:goop/pages/components/goop_drawer.dart';
 import 'package:goop/utils/goop_colors.dart';
 import 'package:goop/utils/goop_images.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:provider/provider.dart';
 import 'package:goop/services/ServiceNotifier.dart';
 
@@ -24,17 +25,29 @@ class HomePage extends StatelessWidget {
         ],
       ),
       drawer: GoopDrawer(),
-      body: Container(),
-
-      // body: Container(
-      //   height: double.infinity,
-      //   width: double.infinity,
-      //   child: Image.asset(
-      //     //TODO: DEVERÁ SER SUBSTITUÍDO PELO MAPA REAL
-      //     GoopImages.map,
-      //     fit: BoxFit.cover,
-      //   ),
-      // ),
+      body: FlutterMap(
+        options: MapOptions(
+          //center: LatLng(51.5, -0.09),
+          zoom: 13.0,
+        ),
+        layers: [
+          TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c']),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                //point: LatLng(51.5, -0.09),
+                builder: (ctx) => Container(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
