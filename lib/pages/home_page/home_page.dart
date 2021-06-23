aed4247b613a32ea9cfd3ba31be9768319671817
Ismael Pedro <http.ismaeel@gmail.com>
2021-06-23 18:36:45 -0300

    Adicionado loading no mapa

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:goop/models/mission.dart';
 import 'package:goop/pages/components/goop_drawer.dart';
 import 'package:goop/pages/components/libComponents.dart';
@@ -20,60 +19,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final serviceNotifier = Provider.of<ServiceNotifier>(context);
+    final controller = MapController();
 
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: GoopColors.red),
-          actions: [
-            Container(
-              width: 45,
-              margin: const EdgeInsets.only(right: 20),
-              child: SvgPicture.asset(GoopImages.red_smile),
-            ),
-          ],
-        ),
-        drawer: GoopDrawer(),
-        body: FutureBuilder(
-            future: serviceNotifier.init(),
-            builder: (context, snapshot) {
-              final _serviceNotifier = Provider.of<ServiceNotifier>(context);
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: GoopColors.red),
+        actions: [
+          Container(
+            width: 45,
+            margin: const EdgeInsets.only(right: 20),
+            child: SvgPicture.asset(GoopImages.red_smile),
+          ),
+        ],
+      ),
+      drawer: GoopDrawer(),
+      body: FutureBuilder(
+        future: serviceNotifier.init(),
+        builder: (context, snapshot) {
+          final _serviceNotifier = Provider.of<ServiceNotifier>(context);
 
-              if (snapshot.connectionState == ConnectionState.done) {
-                return FlutterMap(
-                  options: MapOptions(
-                    // center: LatLng(53, -0.09),
-                    interactiveFlags:
-                        InteractiveFlag.pinchZoom | InteractiveFlag.drag,
-                    center: LatLng(-23.553583043580996, -46.65204460659839),
-                    zoom: 13.0,
-                  ),
-                  layers: [
-                    TileLayerOptions(
-                      urlTemplate:
-                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                      subdomains: ['a', 'b', 'c'],
-                    ),
-                    MarkerLayerOptions(
-                      markers: [
-                        for (MissionModel missionModel
-                            in _serviceNotifier.listMissionModel)
-                          Marker(
-                            width: 40,
-                            height: 80.0,
-                            point: LatLng(
-                                missionModel.establishmentModel.latitude ?? 0,
-                                missionModel.establishmentModel.longitude ?? 0),
-                            builder: (ctx) => Container(
-                              child: SvgPicture.asset(GoopImages.local),
-                            ),
-                          )
-                      ],
-                    ),
+          if (snapshot.connectionState == ConnectionState.done) {
+            return FlutterMap(
+              mapController: controller,
+              options: MapOptions(
+                center: LatLng(-23.553583043580996, -46.65204460659839),
+                interactiveFlags:
+                    InteractiveFlag.pinchZoom | InteractiveFlag.drag,
+                zoom: 13.0,
+              ),
+              layers: [
+                TileLayerOptions(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayerOptions(
+                  markers: [
+                    for (MissionModel missionModel
+                        in _serviceNotifier.listMissionModel)
+                      Marker(
+                        width: 40,
+                        height: 80.0,
+                        point: LatLng(
+                            missionModel.establishmentModel.latitude ?? 0,
+                            missionModel.establishmentModel.longitude ?? 0),
+                        builder: (ctx) => Container(
+                          child: SvgPicture.asset(GoopImages.local),
+                        ),
+                      )
                   ],
-                );
-              } else {
-                return paddingZ(); //??-pedro qual aquele CircularProgress que você coloca?
-              }
-            }));
+                ),
+              ],
+            );
+          } else {
+            return CircularProgressIndicator(
+              strokeWidth: 3,
+              color: GoopColors.red,
+            );
+          }
+        },
+      ),
+    );
   }
 }
