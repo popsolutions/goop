fd7c9772e641c117dd12c78309f7606e278a0d9e
MateusONunes <mateus.2006@gmail.com>
2021-07-12 18:49:18 -0300

    Ajuste simples

--- lib/services/login/user_service.dart
+++ lib/services/login/user_service.dart
@@ -37,7 +37,7 @@ class UserServiceImpl {
       'cnpj_cpf',
       'education_level',
       'gender',
-      // 'missions_count',
+      'missions_count',
       'mobile',
       'email',
       'street',
